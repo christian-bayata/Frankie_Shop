3710c0fa44113c8c453e8f8b6a51981b899a615d
christian-bayata <franksagie1@gmail.com>
2021-11-07 15:06:35 +0100

    refactored database settigns to accomodate different NODE_ENV environments

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 
-const dbConnect = () => {
-    mongoose.connect(process.env.DB_LOCAL_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true
-    })
-    .then(conn => console.log(`Database is connected with host: ${conn.connection.host}`))
+const dbConnect = async (databaseURI) => {
+    try {
+        await mongoose.connect(databaseURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        })
+        .then(() => console.log(`Database is connected to ${databaseURI}`));
+    } catch(err) {
+        console.log('Could not connect to the database', err);
+        process.exit(1)
+    }
 }
-
+    
 module.exports = dbConnect;
